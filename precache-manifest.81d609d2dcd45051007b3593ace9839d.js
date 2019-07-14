5a486d006778ea75ea1230e0a6ebbff7c6031610
Karan Sapolia <sapoliakaran@gmail.com>
2019-07-14 23:24:43 +0530

    Updates

---
+++ precache-manifest.81d609d2dcd45051007b3593ace9839d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55aeaa69a07d5a1460c455fed51f51b4",
+    "url": "/snak3s/index.html"
+  },
+  {
+    "revision": "7cbc678d07442f07f8b6",
+    "url": "/snak3s/static/css/main.3669af14.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/snak3s/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "7cbc678d07442f07f8b6",
+    "url": "/snak3s/static/js/main.e2abbef3.chunk.js"
+  },
+  {
+    "revision": "fff0d5054c2fb092f70a",
+    "url": "/snak3s/static/js/runtime~main.f1fb494b.js"
+  }
+]);
